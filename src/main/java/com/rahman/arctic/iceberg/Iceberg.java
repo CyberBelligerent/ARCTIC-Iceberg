7c5f9ed8c139b651e97674fe082e8bec34be04f1
srahman <seanrahman9@gmail.com>
2024-01-25 20:45:22 +0100

    Initial Commit

---
+++ src/main/java/com/rahman/arctic/iceberg/Iceberg.java
@@ -0,0 +1,12 @@
+package com.rahman.arctic.iceberg;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Iceberg {
+
+	public Iceberg() {
+		System.out.println("Enabling Service: Iceberg");
+	}
+	
+}
