db8ead4d81f0ebea0c7834f6b37881833c0eee45
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-02-18 17:55:05 +0100

    Import Injection
    Iceberg now calls for the creation of Shard and Polarbar as well as adds the component scans for them so that Flock has access to the repos and objects

--- src/main/java/com/rahman/arctic/iceberg/Iceberg.java
+++ src/main/java/com/rahman/arctic/iceberg/Iceberg.java
@@ -1,11 +1,28 @@
 package com.rahman.arctic.iceberg;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Service;
 
+import com.rahman.arctic.polarbear.Polarbear;
+import com.rahman.arctic.shard.Shard;
+
+import lombok.Getter;
+
 @Service
+@EnableJpaRepositories(basePackages = {"com.rahman.arctic.iceberg.repos", "com.rahman.arctic.polarbear.repos", "com.rahman.arctic.shard.repos"})
+@EntityScan(basePackages = {"com.rahman.arctic.iceberg.objects", "com.rahman.arctic.polarbear.objects", "com.rahman.arctic.shard.objects"})
 public class Iceberg {
 
+	@Getter
+	private Shard shard;
+	
+	@Getter
+	private Polarbear polarbear;
+	
 	public Iceberg() {
+		shard = new Shard();
+		polarbear = new Polarbear();
 		System.out.println("Enabling Service: Iceberg");
 	}
 	
