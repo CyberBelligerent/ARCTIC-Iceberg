942a4b9d93c86b5cbf61e23f3a823fec33e7b069
Sean Rahman <157746921+CyberBelligerent@users.noreply.github.com>
2025-12-17 22:03:17 +0100

    Spring Annotation fix
    Iceberg is now just a service and removed @EntityScan and @EnableJpaRepository. This is handled by Flock now

--- src/main/java/com/rahman/arctic/iceberg/Iceberg.java
+++ src/main/java/com/rahman/arctic/iceberg/Iceberg.java
@@ -1,7 +1,5 @@
 package com.rahman.arctic.iceberg;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Service;
 
 import com.rahman.arctic.polarbear.Polarbear;
@@ -10,8 +8,6 @@ import com.rahman.arctic.shard.Shard;
 import lombok.Getter;
 
 @Service
-@EnableJpaRepositories(basePackages = {"com.rahman.arctic.iceberg.repos", "com.rahman.arctic.polarbear.repos", "com.rahman.arctic.shard.repos"})
-@EntityScan(basePackages = {"com.rahman.arctic.iceberg.objects", "com.rahman.arctic.polarbear.objects", "com.rahman.arctic.shard.objects"})
 public class Iceberg {
 
 	@Getter
