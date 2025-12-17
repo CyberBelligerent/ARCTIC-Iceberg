3ffcc16b98147f573927015f825fc7b5b23c7465
Sean Rahman <157746921+CyberBelligerent@users.noreply.github.com>
2025-12-17 22:04:02 +0100

    Spring Boot Object Creation Fix
    Iceberg no longer creates Polarbear or Shard.... Spring Boot does the proper lifecycle hooks for these components instead of Iceberg "newing" them

--- src/main/java/com/rahman/arctic/iceberg/Iceberg.java
+++ src/main/java/com/rahman/arctic/iceberg/Iceberg.java
@@ -2,23 +2,10 @@ package com.rahman.arctic.iceberg;
 
 import org.springframework.stereotype.Service;
 
-import com.rahman.arctic.polarbear.Polarbear;
-import com.rahman.arctic.shard.Shard;
-
-import lombok.Getter;
-
 @Service
 public class Iceberg {
-
-	@Getter
-	private Shard shard;
-	
-	@Getter
-	private Polarbear polarbear;
 	
 	public Iceberg() {
-		shard = new Shard();
-		polarbear = new Polarbear();
 		System.out.println("Enabling Service: Iceberg");
 	}
 	
