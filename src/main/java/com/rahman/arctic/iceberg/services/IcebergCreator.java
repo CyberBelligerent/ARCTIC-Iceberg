8d0c71178177c275cc00ca3b14424aa8d49ca98b
Sean Rahman <157746921+CyberBelligerent@users.noreply.github.com>
2026-03-25 10:13:41 +0100

    Destruction
    Added destroy* functions to help with deleting built environment objects

--- src/main/java/com/rahman/arctic/iceberg/services/IcebergCreator.java
+++ src/main/java/com/rahman/arctic/iceberg/services/IcebergCreator.java
@@ -38,7 +38,10 @@ public class IcebergCreator extends Thread {
 	
 	@Getter @Setter
 	public ShardProfile profile;
-	
+
+	@Getter @Setter
+	private boolean destroyMode = false;
+
 	@Getter
 	private List<ArcticTask<?, ?>> tasksToComplete = new ArrayList<>();
 	
@@ -122,19 +125,104 @@ public class IcebergCreator extends Thread {
 		avso.setName(av.getName());
 		avso.setRangeId(av.getRangeId());
 		avso.setSize(av.getSize());
-		
+
 		sm.getSession(profile).createVolume(avso);
 	}
-	
+
+	public void destroyHost(ArcticHost ah) {
+		ArcticHostSO ahso = new ArcticHostSO();
+		ahso.setName(ah.getName());
+		ahso.setIp(ah.getIp());
+		ahso.setRangeId(ah.getRangeId());
+		ahso.setVolumes(ah.getVolumes());
+		ahso.setNetworks(ah.getNetworks());
+		ahso.setOsType(ah.getOsType());
+		ahso.setExtraVariables(ah.getExtraVariables());
+		ahso.setProviderId(ah.getProviderId());
+
+		sm.getSession(profile).destroyHost(ahso);
+	}
+
+	public void destroyNetwork(ArcticNetwork an) {
+		ArcticNetworkSO anso = new ArcticNetworkSO();
+		anso.setName(an.getNetName());
+		anso.setIpCidr(an.getNetCidr());
+		anso.setIpGateway(an.getGateway());
+		anso.setIpRangeEnd(an.getNetEnd());
+		anso.setIpRangeStart(an.getNetStart());
+		anso.setRangeId(an.getRangeId());
+		anso.setProviderId(an.getProviderId());
+
+		sm.getSession(profile).destroyNetwork(anso);
+	}
+
+	public void destroySecurityGroup(ArcticSecurityGroup asg) {
+		ArcticSecurityGroupSO asgso = new ArcticSecurityGroupSO();
+		asgso.setDescription(asg.getDescription());
+		asgso.setName(asg.getName());
+		asgso.setRangeId(asg.getRangeId());
+		asgso.setProviderId(asg.getProviderId());
+
+		sm.getSession(profile).destroySecurityGroup(asgso);
+	}
+
+	public void destroySecurityGroupRule(ArcticSecurityGroupRule asgr) {
+		ArcticSecurityGroupRuleSO asgrso = new ArcticSecurityGroupRuleSO();
+		asgrso.setDescription(asgr.getDescription());
+		asgrso.setDirection(asgr.getDirection());
+		asgrso.setEndPortRange(asgr.getEndPortRange());
+		asgrso.setEth(asgr.getEth());
+		asgrso.setName(asgr.getName());
+		asgrso.setProtocol(asgr.getProtocol());
+		asgrso.setRangeId(asgr.getRangeId());
+		asgrso.setSecGroup(asgr.getSecGroup());
+		asgrso.setStartPortRange(asgr.getStartPortRange());
+		asgrso.setProviderId(asgr.getProviderId());
+
+		sm.getSession(profile).destroySecurityGroupRule(asgrso);
+	}
+
+	public void destroyRouter(ArcticRouter ar) {
+		ArcticRouterSO arso = new ArcticRouterSO();
+		arso.setConnectedNetworkNames(ar.getNetworks());
+		arso.setName(ar.getName());
+		arso.setRangeId(ar.getRangeId());
+		arso.setProviderId(ar.getProviderId());
+
+		sm.getSession(profile).destroyRouter(arso);
+	}
+
+	public void destroyVolume(ArcticVolume av) {
+		ArcticVolumeSO avso = new ArcticVolumeSO();
+		avso.setBootable(av.isBootable());
+		avso.setDescription(av.getDescription());
+		avso.setImageId(av.getImageId());
+		avso.setName(av.getName());
+		avso.setRangeId(av.getRangeId());
+		avso.setSize(av.getSize());
+		avso.setProviderId(av.getProviderId());
+
+		sm.getSession(profile).destroyVolume(avso);
+	}
+
 	// TODO: Needs to be fixed with user profiles and who is doing this
 	public void run() {
-		tasksToComplete.addAll(sm.getSession(profile).getInstanceTasks().values());
-		tasksToComplete.addAll(sm.getSession(profile).getNetworkTasks().values());
-		tasksToComplete.addAll(sm.getSession(profile).getRouterTasks().values());
-		tasksToComplete.addAll(sm.getSession(profile).getSecurityGroupRuleTasks().values());
-		tasksToComplete.addAll(sm.getSession(profile).getSecurityGroupTasks().values());
-		tasksToComplete.addAll(sm.getSession(profile).getVolumeTasks().values());
-		
+		if (destroyMode) {
+			tasksToComplete.addAll(sm.getSession(profile).getDestroyInstanceTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getDestroyNetworkTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getDestroyRouterTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getDestroySecurityGroupRuleTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getDestroySecurityGroupTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getDestroyVolumeTasks().values());
+		} else {
+			tasksToComplete.addAll(sm.getSession(profile).getInstanceTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getNetworkTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getRouterTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getSecurityGroupRuleTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getSecurityGroupTasks().values());
+			tasksToComplete.addAll(sm.getSession(profile).getVolumeTasks().values());
+		}
+
 		PriorityQueue<ArcticTask<?, ?>> queue = new PriorityQueue<ArcticTask<?, ?>>(tasksToComplete.size(), new Comparator<ArcticTask<?, ?>>() {
 			@Override
 			public int compare(ArcticTask<?, ?> o1, ArcticTask<?, ?> o2) {
@@ -142,11 +230,11 @@ public class IcebergCreator extends Thread {
 			}
 		});
 		queue.addAll(tasksToComplete);
-		
+
 		while(!queue.isEmpty()) {
 			executorService.execute(queue.poll());
 		}
-		
+
 		tasksToComplete.clear();
 	}
 	
